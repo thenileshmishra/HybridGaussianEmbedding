1dd7d64b420b06a6d0af4539ea63311d5d719eec
thenileshmishra <thenileshmishra@gmail.com>
2025-10-18 10:30:00 +0530

    Feature: implement RoBERTa document embedder
    Add RoBertaDocumentEmbedder that combines token, hybrid positional,
    and segment encodings to produce input embeddings for the encoder.

---
+++ src/models/__init__.py
@@ -0,0 +1,10 @@
+from .embedder import RoBertaDocumentEmbedder
+from .roberta_encoder import RoBERTaEncoder
+from .transformer_layers import (
+    PositionwiseFeedForward,
+    MultiHeadedAttention,
+    TransformerEncoderLayer,
+    TransformerInterEncoder,
+)
+from .classifier import Classifier
+from .summary_generator import SummaryGenerator
